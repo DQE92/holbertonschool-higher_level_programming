6bdd2f8515be2eae3152dab0010439dbd7a29a44
DQE92 <duquedylan@icloud.com>
2025-01-29 12:35:08 +0100

    test 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,5 @@
-class Square:
-    """
-    Square: A class that defines a square.
+"""
+Square: A class that defines a square.
     
     This class serves as a basic representation of a square. It allows for future expansion
     to include attributes and methods that describe the properties and behaviors of a square.
@@ -15,5 +14,6 @@ class Square:
         >>> my_square = Square()
         >>> print(type(my_square))
         <class '__main__.Square'>
-    """
-    pass
+"""
+class Square:
+        pass
