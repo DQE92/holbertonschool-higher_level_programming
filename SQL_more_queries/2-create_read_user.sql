9055db1d50a2e218906d8389c6080ac367836b92
DQE92 <duquedylan@icloud.com>
2025-03-06 11:42:56 +0000

    add 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,8 @@
+-- Creates database hbtn_0d_2 if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY user_0d_2_pwd;
-GRANT SELECT ON user_0d_2 TO hbtn_0d_2;
+
+-- Creates user user_0d_2 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grants only SELECT privilege to user_0d_2 on hbtn_0d_2 database
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
