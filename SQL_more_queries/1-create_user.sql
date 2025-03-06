13f57c6ca320969187d6c97ab271753e041f5a47
DQE92 <duquedylan@icloud.com>
2025-03-06 11:17:37 +0000

    add 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Creates MySQL user user_0d_1 with all privileges if it doesn't exist
+-- Creates a new user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grants all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Refresh privileges
+FLUSH PRIVILEGES;
