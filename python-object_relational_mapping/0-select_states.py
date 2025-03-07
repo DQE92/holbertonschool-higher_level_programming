6049d9bd4160cf982412de8e3609a973f206345a
DQE92 <duquedylan@icloud.com>
2025-03-07 13:27:02 +0000

    test 0-1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,56 +1,33 @@
 #!/usr/bin/python3
 """
-This script lists all states from the database hbtn_0e_0_usa.
-It takes three arguments: MySQL username, MySQL password, and database name.
-The results are sorted in ascending order by states.id.
+Script that lists all states from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    """
-    Main function that connects to the MySQL server, retrieves and displays states.
-    """
 
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-    """
-    The MySQLdb.connect() method establishes a connection to the MySQL server.
-    Arguments:
-    - host: The hostname or IP address of the server (localhost)
-    - port: The port number (3306 by default for MySQL)
-    - user: MySQL username
-    - passwd: MySQL password
-    - db: Database name to connect to
-    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    cur = db.cursor()
-    """
-    The cursor object allows us to execute SQL queries and fetch results.
-    """
+    cursor = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    """
-    Executes an SQL query to select all records from the `states` table,
-    ordered by the `id` column in ascending order.
-    """
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    rows = cur.fetchall()
-    """
-    Fetches all the rows resulting from the executed query.
-    """
+    states = cursor.fetchall()
 
-    for row in rows:
-        print(row)
-    """
-    Iterates through the result set and prints each row.
-    """
+    for state in states:
+        print(state)
 
-    cur.close()
+    cursor.close()
     db.close()
-    """
-    Closes the cursor and the database connection to free up resources.
-    """
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
 """
-This script lists all states with a name starting with 'N' from the database hbtn_0e_0_usa.
-It takes three arguments: MySQL username, MySQL password, and database name.
-The results are sorted in ascending order by states.id.
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    """
-    Main function that connects to the MySQL server, retrieves and displays states with names starting with 'N'.
-    """
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
 
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    cursor = db.cursor()
 
-    cur = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
-    """
-    The query selects all rows from the `states` table where the name starts with 'N',
-    using the BINARY keyword to ensure case sensitivity.
-    """
+    states = cursor.fetchall()
 
-    rows = cur.fetchall()
+    for state in states:
+        print(state)
 
-    for row in rows:
-        print(row)
-
-    cur.close()
+    cursor.close()
     db.close()
 
 ~                             
