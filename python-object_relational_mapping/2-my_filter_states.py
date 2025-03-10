4bebeeea40ba8da54961c04f1fc9d98b892bd482
DQE92 <duquedylan@icloud.com>
2025-03-10 15:50:34 +0100

    1

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,37 +6,14 @@ It takes four arguments: MySQL username, MySQL password, database name,
 and state name searched.
 The results are sorted in ascending order by states.id.
 """
-import MySQLdb
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """
-    Main function that connects to the MySQL server, retrieves, and displays states
-    where name matches the argument.
-    """
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-
-    cur = db.cursor()
-
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
-    cur.execute(query)
-    """
-    The query selects all rows from the `states` table where the name matches the user input.
-    The BINARY keyword ensures case-sensitive comparison.
-    The format() method inserts user input into the query.
-    """
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
-
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
