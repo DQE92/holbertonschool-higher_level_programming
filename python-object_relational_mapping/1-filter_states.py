74c7b86dfa04a0eea11b3dd5db813c97a405eb4a
DQE92 <duquedylan@icloud.com>
2025-03-17 19:38:43 +0000

    test1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,48 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+""" List all states with a name starting with N from the hbtn_0e_0_usa """
+
+
 import sys
 import MySQLdb
 
+
+def list_states():
+
+    # Retrieving arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    cursor = None
+    db = None
+
+    try:
+        # Database connexion
+        db = MySQLdb.connect(host="localhost", user=mysql_username,
+            passwd=mysql_password, db=database_name, port=3306)
+
+        #cursor creation
+        cursor = db.cursor()
+
+        #Execute SQL query to retrieve reports sorted by id
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+
+        # Retrieving and displaying results
+        states = cursor.fetchall()
+        if states:
+            for state in states:
+                print(state)
+
+
+    except MySQLdb.Error as e:
+        print(f"SQL connection or execution error : {e}")
+
+    finally:
+        # Close to connexion
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    list_states()
