46981742239b7d9a60c63abbe6986e54e90375d1
DQE92 <duquedylan@icloud.com>
2025-03-21 23:45:39 +0100

    test task 2

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """
+    Renders the index.html template when the home route is accessed.
+    """
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    """
+    Renders the about.html template when the about route is accessed.
+    """
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    """
+    Renders the contact.html template when the contact route is accessed.
+    """
+    return render_template("contact.html")
+
+@app.route('/items')
+def show_items():
+    with open("items.json", "r") as file:
+        data = json.load(file)
+        
+        items = data.get("items, []")
+        
+        return render_template("items.html", items=items)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
